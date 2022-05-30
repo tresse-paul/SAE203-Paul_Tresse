a442ffc55bd59572423bf5f5cb954dea0f9956bd
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-30 18:45:52 +0200

    url image

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -7,9 +7,9 @@
         Laissez-vous emporter par les artistes <strong>les plus prometteurs</strong> venus des quatre coins du globe.
       </p>
       <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-10">
-        <ArtisteCard nom="Qrion" image="/src/assets/Qrion.jpeg" date="Vendredi 5 août 2022" />
-        <ArtisteCard nom="Madeon" image="/src/assets/Madeon.jpeg" date="Samedi 6 août 2022" />
-        <ArtisteCard nom="Sarah Landry" image="/src/assets/Sarah Landry.jpeg" date="Dimanche 7 août 2022" />
+        <ArtisteCard nom="Qrion" image="../../src/assets/Qrion.jpeg" date="Vendredi 5 août 2022" />
+        <ArtisteCard nom="Madeon" image="../../src/assets/Madeon.jpeg" date="Samedi 6 août 2022" />
+        <ArtisteCard nom="Sarah Landry" image="../../src/assets/Sarah_Landry.jpeg" date="Dimanche 7 août 2022" />
       </div>
       <div class="flex justify-center">
         <bouton principal>Voir plus</bouton>
