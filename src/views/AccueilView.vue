84ec05c61a28e2f655e054f328dfaefd664a6cfa
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-30 08:36:42 +0200

    style personnalisé tailwind

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <h1 class="text-2xl">Electronic Palace Festival</h1>
+    <div class="h-10 w-10 bg-shadow-card"></div>
   </main>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
       "colors": {
         "gray": {
           "1000": "#0f0f0f"
-        }
+        },
       },
+
+      backgroundImage: {
+        'shadow-card': "linear-gradient(to bottom, rgba(0,0,0,0) 39.06%, rgba(15,15,15,0.8) 100%)",
+      },
+
       "fontFamily": {
         "barlow": "Barlow",
         "work-sans": "Work Sans"
