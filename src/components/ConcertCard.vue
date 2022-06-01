200fc25e55bcfa5b4afcf8586d0e745d6be8757c
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-06-01 08:34:39 +0200

    modification des hover card artistes

--- src/components/ConcertCard.vue
+++ src/components/ConcertCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-12 items-center justify-between rounded-full bg-gray-50 px-3.5">
+  <div class="flex h-12 items-center justify-between rounded-full bg-gray-50 px-3.5 hover:-translate-y-1">
     <span class="h-3.5 w-3.5 rounded-full bg-red-500"></span>
     <p class="font-barlow text-xl text-gray-1000">{{ nom }}</p>
     <Chevron class="fill-red-500" />
