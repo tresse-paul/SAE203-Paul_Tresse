eb2fce7e7b567abf20d05a87d15146e705bda510
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-06-01 08:18:32 +0200

    menu hamburger

--- src/components/Entete.vue
+++ src/components/Entete.vue
@@ -1,8 +1,26 @@
 <template>
   <nav class="flex h-16 w-screen items-center justify-between bg-gray-1000 px-5 xl:hidden">
     <RouterLink to="/"><LogoMobile class="fill-gray-50" /></RouterLink>
-    <NavBouton class="fill-gray-50" />
+    <div @click="toggleNav" class="flex">
+      <button type="button" class="text-gray-100 hover:text-gray-400 focus:text-gray-400 focus:outline-none">
+        <NavBouton class="fill-gray-50" />
+      </button>
+    </div>
   </nav>
+  <ul :class="showMenu ? 'flex' : 'hidden'" class="xl:hiden my-4 flex flex-col items-end gap-5 px-5">
+    <li>
+      <RouterLink to="/programmation" class="font-work-sans text-xl font-bold uppercase text-gray-50">Programmation</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/artistes" class="font-work-sans text-xl font-bold uppercase text-gray-50">Les artistes</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/festival" class="font-work-sans text-xl font-bold uppercase text-gray-50">Le festival</RouterLink>
+    </li>
+    <li>
+      <RouterLink to="/contact" class="font-work-sans text-xl font-bold uppercase text-gray-50">Contact</RouterLink>
+    </li>
+  </ul>
 
   <nav class="hidden h-32 w-screen items-center justify-between bg-gray-1000 px-12 xl:flex">
     <RouterLink to="/"><LogoDefault class="fill-gray-50" /></RouterLink>
@@ -19,8 +37,14 @@
 import LogoMobile from "./icons/LogoMobile.vue";
 import LogoDefault from "./icons/LogoDefault.vue";
 import NavBouton from "./icons/NavBouton.vue";
+import { ref } from "vue";
 
 export default {
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
   components: { LogoMobile, LogoDefault, NavBouton },
 };
 </script>
