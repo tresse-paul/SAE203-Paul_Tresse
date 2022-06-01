3156932b387d6b866bd6d7fe10773651850e5cdd
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-06-01 09:10:36 +0200

    404 page

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import FestivalView from '../views/FestivalView.vue'
 import ContactView from '../views/ContactView.vue'
 import LegaleView from '../views/LegaleView.vue'
 
+import PageNotFoundView from '../views/PageNotFoundView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,6 +17,8 @@ const router = createRouter({
     { path: '/festival', name: 'Festival', component: FestivalView },
     { path: '/contact', name: 'Contact', component: ContactView },
     { path: '/legale', name: 'Legale', component: LegaleView },
+
+    { path: '/:pathMatch(.*)*', name: 'PageNotFound', component: PageNotFoundView },
     // ici les autre routes
   ]
 })
---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex h-screen flex-col items-center gap-20 bg-hero-bg-sec py-20 px-5 md:py-20 xl:px-32">
+    <h1 class="text-center font-work-sans text-5xl font-bold text-gray-50 md:text-6xl">Désolé, cette page n'existe pas !</h1>
+    <RouterLink to="/"><bouton principal>Accueil</bouton></RouterLink>
+    <input type="search" class="w-full rounded-lg focus:outline-none focus:ring-red-500" placeholder="Ma recherche..." />
+  </div>
+  <Pieds />
+</template>
+
+<script>
+import bouton from "../components/bouton.vue";
+import Pieds from "../components/Pieds.vue";
+
+export default {
+  components: { bouton, Pieds },
+};
+</script>
